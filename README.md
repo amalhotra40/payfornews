27f201a5a5886fa639730679c3eb2dd4512d1e12
Abhinav Malhotra <abhinav.email.id@gmail.com>
2023-12-28 19:41:12 -0500

    app final cleanup

--- README.md
+++ README.md
@@ -6,12 +6,6 @@ A [research paper](https://policydialogue.org/publications/working-papers/paying
 
 This web-application makes it easy to calculate the "fair payment" for other markets using the methodology described in the above paper.
 
-## Disclaimer
+## Disclaimers
 
- The purpose of this application is to aid researchers to calculate numbers for their own countries and no gurantee about the accuracy of the generated "fair payment" should be assumed or expected. Furthermore, note that all data, values, methodology, and assumptions used in the underlying code come directly from the above source paper. The author of this web application is not responsible for any omissions in the underlying paper.
-
-## Contacts
-
-Please contact [Haaris Mateen](mailto:hmateen@uh.edu) (the lead author of the study) for any questions or comments on the original research, including the methodology and assumptions, or if you would like to understand more about the inputs and outputs of this web application.
-
-If you find any bugs while using the application or discrepancies between the original paper and this web application, please [email here](mailto:abhinav.email.id+payingfornewswebapp@gmail.com) instead.
+ The purpose of this application is to aid researchers to calculate numbers for their own countries and no gurantee about the accuracy of the generated "fair payment" should be assumed or expected. Furthermore, note that all data, values, methodology, and assumptions used in the underlying code come directly from the above source paper. The author of this web application is not responsible for any omissions/assumptions in the source paper. Please contact [Haaris Mateen](mailto:hmateen@uh.edu) (the lead author of the source research) for any questions or comments on the original research, including the methodology and assumptions, or if you would like to understand more about the inputs and outputs of this web application. If you find any bugs while using the application or discrepancies between the original paper and this web application, please [email here](mailto:abhinav.email.id+payingfornewswebapp@gmail.com) instead.
--- app/main.py
+++ app/main.py
@@ -85,24 +85,12 @@ total = google + meta
 st.markdown(
     'A [research paper](https://policydialogue.org/publications/working-papers/paying-for-news-what-google-and-meta-owe-us-publishers-draft-working-paper/) titled _Paying for News: What Google and Meta Owe US Publishers_ by Patrick Holder, Haaris Mateen, Anya Schiffrin, and Haris Tabakovic estimates the payment that Facebook and Google Search platforms would owe to news publishers for the use of news content, if the Journalism Competition & Preservation Act (JCPA) comes into force using a novel methodology. This web-application makes it easy to calculate the "fair payment" for other markets using the methodology described in the above paper.'
 )
-
-
-st.write("----")
-st.subheader("Modify assumptions on the left to calculate the estimated payments below")
-st.subheader(
-    "Estimated payments: ",
-    help=f"'Fair payment' from platforms to publishers; see reference: {REFERENCE} for methodology",
-)
+st.markdown("#### Modify inputs in sidebar 👈 to estimate 'fair payment' ⬇️")
 st.write(f"Google: ", round(google, 2), f"million {currency}.")
 st.write(f"Facebook: ", round(meta, 2), f"million {currency}.")
 st.write(f"Sum: ", round(total, 2), f"million {currency}.")
 st.divider()
-st.subheader("Disclaimer:")
-st.markdown(
-    'The purpose of this application is to aid researchers to calculate numbers for their own countries and no gurantee about the accuracy of the generated "fair payment" should be assumed or expected. Furthermore, note that all data, values, methodology, and assumptions used in the underlying code come directly from the above source paper. The author of this web application is not responsible for any omissions in the underlying paper.'
-)
 
-st.subheader("Contact:")
-st.markdown(
-    "Please contact [Haaris Mateen](mailto:hmateen@uh.edu) (the lead author of the study) for any questions or comments on the original research, including the methodology and assumptions, or if you would like to understand more about the inputs and outputs of this web application. If you find any bugs while using the application or discrepancies between the original paper and this web application, please [email here](mailto:abhinav.email.id+payingfornewswebapp@gmail.com) instead."
+st.warning(
+    """*Disclaimers:* The purpose of this application is to aid researchers to calculate numbers for their own countries and no gurantee about the accuracy of the generated "fair payment" should be assumed or expected. Furthermore, note that all data, values, methodology, and assumptions used in the underlying code come directly from the above source paper. The author of this web application is not responsible for any omissions/assumptions in the source paper. Please contact [Haaris Mateen](mailto:hmateen@uh.edu) (the lead author of the source research) for any questions or comments on the original research, including the methodology and assumptions, or if you would like to understand more about the inputs and outputs of this web application. If you find any bugs while using the application or discrepancies between the original paper and this web application, please [email here](mailto:abhinav.email.id+payingfornewswebapp@gmail.com) instead."""
 )
